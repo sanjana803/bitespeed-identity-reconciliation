3291dd725ef439e2c31e48f965140ca75189736d
Kumari Sanjana <kumarisanjana@192.168.1.11>
2025-06-10 05:18:51 +0530

    fix: remove unique constraints from email and phoneNumber fields

---
+++ prisma/migrations/20250609234846_remove_unique_constraints/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Contact_email_idx";
+
+-- DropIndex
+DROP INDEX "Contact_email_key";
+
+-- DropIndex
+DROP INDEX "Contact_linkedId_idx";
+
+-- DropIndex
+DROP INDEX "Contact_phoneNumber_idx";
+
+-- DropIndex
+DROP INDEX "Contact_phoneNumber_key";
